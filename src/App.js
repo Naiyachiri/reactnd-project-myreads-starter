84e092bc2a956df3213f5ff22248b6f1af8f30c5
James Bui <buijvbio@gmail.com>
2018-08-09 15:49:28 -0400

    FEAT: separated search component
    Is now search.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks.js'
-
+import SearchList from'./Search.js'
 
 class BooksApp extends React.Component {
   state = {
@@ -25,6 +25,11 @@ class BooksApp extends React.Component {
       this.setState({ bookData })
     }))
   }
+
+  handleSearchClose = () => {
+    this.setState({showSearchPage: false})
+  }
+
   //set a local prop to update our server based on changes on a child component
   // an array is passed with the [0] = bookID and [1] = shelf
   updateBook = (book, shelf) => {
@@ -47,26 +52,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchList handleSearchClose={this.handleSearchClose}/>
         ) : (
           <div className="list-books">
             <div className="list-books-title">
